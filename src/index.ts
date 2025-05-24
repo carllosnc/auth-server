7a09944ec92aeab25c0fe0871ccb45bb539574b9
Carlos Costa <carllos.nc@gmail.com>
2025-05-24 03:14:25 -0300

    Fix environment variable checks and improve CORS configuration in API routes

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ app.get('/', (c) => {
 	let isEnvSet = false
 
 	if (
-		!c.env.BETTER_AUTH_SECRET ||
-		!c.env.BETTER_AUTH_URL ||
-		!c.env.TURSO_DATABASE_URL ||
-		!c.env.TURSO_AUTH_TOKEN ||
-		!c.env.AUTH_GOOGLE_ID ||
-		!c.env.AUTH_GOOGLE_SECRET
+		c.env.BETTER_AUTH_SECRET &&
+		c.env.BETTER_AUTH_URL &&
+		c.env.TURSO_DATABASE_URL &&
+		c.env.TURSO_AUTH_TOKEN &&
+		c.env.AUTH_GOOGLE_ID &&
+		c.env.AUTH_GOOGLE_SECRET
 	) {
 		isEnvSet = true
 	}
@@ -26,30 +26,34 @@ app.get('/', (c) => {
 	})
 })
 
-app.use(
-	"/api/auth/**", // or replace with "*" to enable cors for all routes
+app.use( "/api/auth/**", async (c, next) => {
+	const productionOrigins = c.env.PRODUCTION_ORIGINS.split(',')
+
 	cors({
 		origin: [
-			...origins
+			...productionOrigins,
+			...origins,
 		],
 		allowHeaders: ["Content-Type", "Authorization"],
 		allowMethods: ["POST", "GET", "OPTIONS"],
 		exposeHeaders: ["Content-Length"],
 		maxAge: 600,
 		credentials: true,
-	}),
- )
+	})
 
- app.on(["POST", "GET"], "/api/auth/**", async (c) => {
-  const auth = betterAuthSettings({
-    tursoUrl: c.env.TURSO_DATABASE_URL,
-    tursoToken: c.env.TURSO_AUTH_TOKEN,
-    authGoogleId: c.env.AUTH_GOOGLE_ID,
-    authGoogleSecret: c.env.AUTH_GOOGLE_SECRET,
+	await next()
+})
+
+app.on(["POST", "GET"], "/api/auth/**", async (c) => {
+	const auth = betterAuthSettings({
+		tursoUrl: c.env.TURSO_DATABASE_URL,
+		tursoToken: c.env.TURSO_AUTH_TOKEN,
+		authGoogleId: c.env.AUTH_GOOGLE_ID,
+		authGoogleSecret: c.env.AUTH_GOOGLE_SECRET,
   })
 
-  const result = await auth.handler(c.req.raw)
-  return result
+	const result = await auth.handler(c.req.raw)
+	return result
 })
 
 export default app
